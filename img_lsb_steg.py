de1070024bced7c5f0d06119d32a3e852464efc8
2CS\anhnguyen <danguyen@2clicksolutions.onmicrosoft.com>
2018-03-27 11:08:31 +0700

    trying to open file into bytes targetting BMP format exclude header and read text size (not input => need to be changed to user input) to write it into img data

--- img_lsb_steg.py
+++ img_lsb_steg.py
@@ -1,4 +1,5 @@
-import binascii
+HEADER_SIZE = 54 # Gia tri header cua file bmp
+DELIMITER = "$" # Phan cach giua gia tri so ki tu can giau vao hinh va message
 
 def hex2rgb(hexparam):
     return tuple(bytearray.fromhex(hexparam[1:]))
@@ -11,15 +12,31 @@ def bin2str(binary):
     mess = binascii.unhexlify('%x'%(int('0b'+binary, 2)))
     return mess.decode()
 
-# red = input('Please input red: ')
-# green = input('Please input green: ')
-# blue = input('Please input blue: ')
+class GiautinLSB(object):
 
-# result = rgb2hex(red, green, blue)
-# print(result)
-# vax = hex2rgb(result)
-# print(vax)
-string = input('fuck you:')
-val1 = str2bin(string)
-print(val1)
-print(bin2str(val1))
+    def __init__(self):
+        self.bytes_processed = 0
+        self.new_image_data = bytearray()
+        self.original_image = ''
+        self.text_to_hide = ''
+
+    def open_image(self):
+        # Mo file de xu ly
+        with open(ImageFile, "rb") as f:
+            self.original_image = f.read()
+
+    # Doc va ghi lai phan header vao file moi (chi ghi vao phan data)
+    def read_header(self):
+        for x in range(0, HEADER_SIZE):
+            self.new_image_data.append(self.original_image[x])
+            self.bytes_processed += 1
+
+    def hide_text_size(self):
+        sz = len(self.text_to_hide)
+        processed_text_size = str(sz)
+        processed_text_size += DELIMITER # processed_text_size = so luong ky tu + $ (VD: 18$...)
+        self.hide(processed_text_size)
+
+    # Giau mess vao trong anh dung LSB
+    def hide(self, mess):
+        
